722eba2d0247608ae3c4c143a8e44271c571295f
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 10:53:22 -0500

    Committing code that asks for the user's question then gives them a response.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String userQuestion = "";
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What kind of programming question would you like to ask the Rubber Duck program");
+        userQuestion = myScanner.nextLine();
+
+        if (userQuestion.length() < 200) {
+            System.out.println("Ask a better question?");
+        } else if (userQuestion.length() > 500) {
+            System.out.println("Simplify your question!");
+        } else if (userQuestion.length() >= 200 && userQuestion.length() <= 500){
+            System.out.println("Quack!  Quack!");
+        }
     }
 }
